1ddc852b15729e1407c1dbec67e59910ceeed932
Chairuni Aulia Nusapati <chairuni.aulia.nusapati@gmail.com>
2016-04-05 13:52:29 +0700

    Initial Commit

---
+++ BaseOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BaseOpenGL
+//
+//  Created by Chairuni Aulia Nusapati on 4/5/16.
+//  Copyright © 2016 Chairuni Aulia Nusapati. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
